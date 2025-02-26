16bd237567428fcc9604484d34ecbc367b4351f7
hectorpc <100495794@alumnos.uc3m.es>
2025-02-26 23:10:38 +0100

    Rama de base de datos para probar

--- src/server-side/servidor-mq.c
+++ src/server-side/servidor-mq.c
@@ -5,7 +5,13 @@
 #include<mqueue.h>
 #include<stdlib.h>
 #include "../claves.h"
+#include<sqlite3.h>
 #define MAX_THREADS 10
+
+
+
+
+
 
 
 
@@ -30,20 +36,114 @@ typedef struct request {
     struct Coord value_3;
 }request;
 
+typedef struct parameters_to_pass{
+  request *this_request;
+  int identifier;
 
 
-void process_request(request *this_request) {
+}parameters_to_pass;
+
+
+
+void process_request(parameters_to_pass *parameters) {
     pthread_mutex_lock(&mutex);
-    request local_request = *this_request;
+    request local_request = *parameters->this_request;
+    int local_id = parameters->identifier;
     free_mutex_cond = 1;
     pthread_mutex_unlock(&mutex);
 }
 
 
 
-int main(int argc, char *argv[]) {
-    char nombre[20] = "/servidor_queue_9453";
+void create_table(sqlite3 *db){
+    char *message_error = NULL;
+    char *new_table =
+        "CREATE TABLE IF NOT EXISTS data("
+        " data_key INTEGER PRIMARY KEY,"
+        " value1 TEXT,"
+        " x INTEGER,"
+        " y INTEGER"
+        ");";
+    if (sqlite3_exec(db, new_table, NULL, NULL, &message_error) != SQLITE_OK){
+      fprintf(stderr, "ERROR CREANDO TABLA 1\n");
+      exit(-4);
+    }
+    message_error = NULL;
+    new_table =
+        "CREATE TABLE IF NOT EXISTS value2_all("
+        " id TEXT PRIMARY KEY,"
+        " data_key INTEGER,"
+        " value REAL,"
+        "CONSTRAINT fk_origin FOREIGN KEY(data_key) REFERENCES data(pk) ON DELETE CASCADE ON UPDATE CASCADE"
+        ");";
+    if (sqlite3_exec(db, new_table, NULL, NULL, &message_error) != SQLITE_OK){
+        fprintf(stderr, "ERROR CREANDO TABLA 2\n");
+        exit(-4);
+    }
+}
 
+
+void insert_data_TEST(sqlite3 *db){
+    char *error_message = NULL;
+    char insert[256];
+
+    //Insertar los primeros parametros en data
+    sprintf(insert,
+        "INSERT into data(data_key, value1,x,y) "
+        " VALUES(%d, '%s', %d ,%d)",3, "SEXO",5, 5);
+    printf("Esto vale insert: %s\n", insert);
+    int test;
+    if ((test =sqlite3_exec(db, insert, NULL, NULL, &error_message)) != SQLITE_OK){
+        if (test != SQLITE_CONSTRAINT){
+        fprintf(stderr, "ERROR insertando en TABLA\n");
+        exit(-4);
+        }
+        else{
+          printf("DUPLICADA LA FK BOBO\n");
+        }
+    }
+
+    //insertar value2
+    double var2[3] = {3.44, 4.3, 5.5};
+    char primary_key[20];
+    for(int i=0; i<3; i++){
+        sprintf(primary_key, "%d%d", 3, i);
+        sprintf(insert,
+        "INSERT into value2_all(id,data_key,value) "
+        " VALUES(%s, %d, %f)",primary_key, 3, var2[i]);
+        printf("Esto vale insert: %s\n", insert);
+        if ((test =sqlite3_exec(db, insert, NULL, NULL, &error_message)) != SQLITE_OK){
+            if (test != SQLITE_CONSTRAINT){
+                fprintf(stderr, "ERROR insertando en TABLA\n");
+                exit(-4);
+            }
+            else{
+                printf("DUPLICADA LA FK BOBO\n");
+            }
+        }
+    }
+}
+
+
+int main(int argc, char *argv[]) {
+
+    //Creando e inicializando la base de datos
+    sqlite3 *database;
+    int create_database = sqlite3_open("database.db", &database);
+    if (create_database != SQLITE_OK){
+        fprintf(stderr,"Error opening the database\n");
+        exit(-1);
+    }
+    printf("Exito abriendo la base de datos\n");
+
+    //Creo la tabla principal "data"
+    create_table(database);
+    insert_data_TEST(database);
+
+
+
+
+    //Creación de hilos, arrays de hilos, y mutex y avriables condicion
     pthread_t thread_array[MAX_THREADS];
     int free_threads_array[MAX_THREADS];
     for(int i =0; i< MAX_THREADS; i++) {
@@ -52,15 +152,20 @@ int main(int argc, char *argv[]) {
     pthread_mutex_init(&mutex, NULL);
     pthread_cond_init(&cond_wait, NULL);
 
+
+    //Inicializo y abro la cola del servidor
     mqd_t server_queue;
+    char nombre[20] = "/servidor_queue_9453";
     server_queue = mq_open(nombre, O_CREAT|O_WRONLY, 0700);
     if (server_queue == -1) {
         mq_close(server_queue);
-        printf("Error\n");
+        printf("Error abriendo la cola del servidor\n");
         return -1;
     }
-    printf("Todo bien %d\n", server_queue);
+    printf("Todo bien abriendo la cola del servidor con fd: %d\n", server_queue);
 
+
+    //Gestion de la concurrencia con las peticiones
     while(1) {
         request new_request;
         if (mq_receive(server_queue,(char *) &new_request, sizeof(new_request),0 ) >= 0) {
