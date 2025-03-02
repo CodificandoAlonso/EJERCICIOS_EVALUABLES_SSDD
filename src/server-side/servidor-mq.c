e80a2eec86500943d1df642971ea245ca88ab5fc
hectorpc <100495794@alumnos.uc3m.es>
2025-03-02 21:03:11 +0100

    eyeyey pequeña no digas eso

--- src/server-side/servidor-mq.c
+++ src/server-side/servidor-mq.c
@@ -1,3 +1,4 @@
+#define _GNU_SOURCE
 #include<stdio.h>
 #include<pthread.h>
 #include "../claves.h"
@@ -9,15 +10,30 @@
 #include <string.h>
 #include<unistd.h>
 
+
 #define MAX_THREADS 10
+
+
+
 
 
 //Creación de hilos, arrays de hilos y contador de hilos ocupados
 pthread_t thread_pool[MAX_THREADS];
 int free_threads_array[MAX_THREADS];
 int workload = 0;
+int free_mutex_copy_params_cond = 0;
+pthread_mutex_t mutex_copy_params;
+pthread_cond_t cond_wait_cpy;
+pthread_mutex_t mutex_threads;
+pthread_cond_t cond_wait_threads;
 
+
+
+/**@brief Esta función se usa para rellenar el array auxiliar que indica qué hilo está trabajando(1)
+ *y cuál está libre(0)
+ */
 void pad_array()
+
 {
     for (int i = 0; i < MAX_THREADS; i++)
     {
@@ -25,12 +41,6 @@ void pad_array()
     }
 }
 
-
-int free_mutex_copy_params_cond = 0;
-pthread_mutex_t mutex_copy_params;
-pthread_cond_t cond_wait_cpy;
-pthread_mutex_t mutex_threads;
-pthread_cond_t cond_wait_threads;
 
 //LA COLA ACABA CON 9453
 
@@ -57,6 +67,13 @@ typedef struct parameters_to_pass
 } parameters_to_pass;
 
 
+/**@brief Esta es la función que ejecutan los distintos hilos dentro de nuestra pool de hilos
+ *Requiere de la dir de memoria de uns estructura de tipo parameters_to_pass que se compone de
+ *una estructura petición y una variable id, que identifica al thread que está ejecutando, para
+ *indicar en el array de threads que ese hilo no está disponible en el momento. Una vez realiza la copia
+ *local de los datos se encargará de gestionar las distintas llamadas de claves.c para realizar las gestiones
+ *en la base de datos correspondiente
+ */
 void process_request(parameters_to_pass* parameters)
 {
     pthread_mutex_lock(&mutex_copy_params);
@@ -65,17 +82,29 @@ void process_request(parameters_to_pass* parameters)
     free_mutex_copy_params_cond = 1;
     free_threads_array[local_id] = 1;
     printf("Hilo %d ocupado\n", local_id);
+    pthread_cond_signal(&cond_wait_cpy);
     pthread_mutex_unlock(&mutex_copy_params);
     sleep(10);
-    pthread_mutex_lock(&mutex_threads);
-    free_threads_array[local_id] = 0;
-    workload--;
+
     printf("Acabado el trabajo el hilo %d\n", local_id);
-    pthread_mutex_unlock(&mutex_threads);
     pthread_exit(0);
 }
 
 
+
+
+/**@brief Función implementada al inicializarse el servidor que creará las tablas de SQL que se encargarán
+ *de mantener nuestros datos ordenados. Hemos creado 2 tablas, una "data" que guardará tanto el id(Primary key)
+ *como value1 y value3. Value2 como es un array de longitud variable, nos hemos creado una tabla "value2_all"
+ *que hereda de data la PK a modo de Foreign Key con UPDATE y DELETE CASCADE(Si se borra la pk de data, se borrarán
+ *todas las referencias a ella en "value2-all". La pk de esta tabla será para cada elemento del array, la conversión
+ *a entero de la concatenación de la PK con el índice del elemento en el array. Por ejemplo, si para id 3 tengo que
+ *insertar el vector {3.44, 2.15, 14.33} tendré 3 filas en esta nueva tabla
+ *  PK   FK   VALUE
+ *  30   3    3.44
+ *  31   3    2.15
+ *  32   3    14.33
+ */
 void create_table(sqlite3* db)
 {
     char* message_error = NULL;
@@ -107,6 +136,9 @@ void create_table(sqlite3* db)
 }
 
 
+
+/**@brief PRUEBA. HABRA QUE MIGRARLA A CLAVES.C
+ */
 void insert_data_TEST(sqlite3* db)
 {
     char* error_message = NULL;
@@ -127,8 +159,6 @@ void insert_data_TEST(sqlite3* db)
         }
         printf("DUPLICADA LA PK BOBO\n");
     }
-
-    //insertar value2
     double var2[3] = {3.44, 4.3, 5.5};
     char primary_key[20];
     for (int i = 0; i < 3; i++)
@@ -149,6 +179,8 @@ void insert_data_TEST(sqlite3* db)
         }
     }
 }
+
+
 
 
 int main(int argc, char* argv[])
@@ -202,19 +234,40 @@ int main(int argc, char* argv[])
 
 
     request new_request;
+    char buffer[10];
     //Gestion de la concurrencia con las peticiones
     while (1)
     {
+        printf("PARA SALIR PON 'exit' mamawebo\n");
+        fgets(buffer,sizeof(buffer),stdin);
+        if (strcmp(buffer, "exit"))
+        {
+            break;
+        }
+
+        //Esto intenta realizar un join no bloqueante
+        pthread_mutex_lock(&mutex_threads);
+        for (int i = 0; i < MAX_THREADS; i++) {
+            if (free_threads_array[i] == 1) { //Si el hilo ha estado trabajando miro a ver si puedo hacerle join
+                if (pthread_tryjoin_np(thread_pool[i], NULL)== 0) {
+                    free_threads_array[i] = 0; //Al ruedo de nuevo, maquina
+                    workload--; // Avisar de que se puede currar
+                    printf("Hilo %d liberado y listo para reutilizarse\n", i);
+                }
+            }
+        }
+        pthread_mutex_unlock(&mutex_threads);
+
+
         ssize_t message = mq_receive(server_queue, (char*)&new_request, sizeof(request), 0);
         if (message >= 0)
         {
             printf("Se ha recibido un mensaje con id %d\n", new_request.key);
+
+
+            //Miro el primer hilo disponible y le mando currar. JAAAAPIUU, a currar esclavo
+            //Necesita seccion critica por el acceso al array
             pthread_mutex_lock(&mutex_threads);
-            while(workload ==MAX_THREADS)
-            {
-                pthread_cond_wait(&cond_wait_threads, &mutex_threads);
-            }
-            pthread_mutex_unlock(&mutex_threads);
             for(int i =0; i< MAX_THREADS; i++)
             {
                 if(free_threads_array[i] == 0){
@@ -224,23 +277,30 @@ int main(int argc, char* argv[])
                     pthread_create(&thread_pool[i],NULL,(void *)process_request,&params);
                     break;
                 }
-
             }
-            pthread_mutex_lock(&mutex_copy_params);
-            while(free_mutex_copy_params_cond == 0)
-                pthread_cond_wait(&cond_wait_cpy, &mutex_copy_params);
-            free_mutex_copy_params_cond = 0;
-            pthread_mutex_unlock(&mutex_copy_params);
+            pthread_mutex_unlock(&mutex_threads);
 
+
+            //Solo hago la espera si no hay ningun hilo currando. Si son todos unos putos vagos de mierda no entro
+            pthread_mutex_lock(&mutex_threads);
+            if (workload >0)
+            {
+                pthread_mutex_lock(&mutex_copy_params);
+                while(free_mutex_copy_params_cond == 0)
+                    pthread_cond_wait(&cond_wait_cpy, &mutex_copy_params);
+                free_mutex_copy_params_cond = 0;
+                pthread_mutex_unlock(&mutex_copy_params);
+            }
+            else
+            {
+                pthread_mutex_unlock(&mutex_threads);
+            }
         }
         else {
+            //si se me lia el mensaje
             printf("error al recibir, tonto\n");
             //DE MOMENTO SI ES ERROR EN COMUNICACION ES ERROR -2
             exit(-2);
-        }
-        for(int i =0; i<MAX_THREADS; i++)
-        {
-            pthread_join(thread_pool[i], NULL);
         }
     }
     return 0;
