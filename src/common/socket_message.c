5bc43d994ffadc484cedf11ae7e3f93778e4cef3
hectorpc <100495794@alumnos.uc3m.es>
2025-03-30 13:19:30 +0200

    SOCKETS

--- src/common/socket_message.c
+++ src/common/socket_message.c
@@ -4,12 +4,59 @@
 #include "socket_message.h"
 #include "struct.h"
 #include <stdio.h>
+#include <string.h>
 #include <unistd.h>
+#include <netinet/in.h>
 
-int receive_message(int socket, request *message) {
+
+int isBigEndian(void) {
+    unsigned int num = 1;
+    // Si el primer byte (dirección más baja) es 1, es little endian
+    if (*(char *)&num == 1)
+        return 0;  // little endian
+    else
+        return 1;  // big endian
+}
+
+double swap_endian(double value) {
+    return *(double*)((uint64_t[]){ __builtin_bswap64(*(uint64_t*)&value) });
+}
+
+
+double host_to_net_double(double value)
+{
+    if (isBigEndian() == 0) //Little Endian
+    {
+        return swap_endian(value);
+    }
+    return value;
+}
+
+void print_double_hex(double value) {
+    uint8_t bytes[8];
+    memcpy(bytes, &value, sizeof(bytes));
+    for (int i = 0; i < 8; i++) {
+        printf("%02X ", bytes[i]);
+    }
+    printf("\n");
+}
+
+double net_to_host_double(double value)
+{
+    if (isBigEndian() == 0) //Little Endian
+    {
+        return swap_endian(value);
+    }
+    return value;
+}
+
+
+
+int receive_package(int socket,void *message, int size)
+{
     int r = 0;
-    int left = sizeof(request);
-    char *buffer = (char *) message;
+    int left = size;
+    void *buffer = message;
 
     while (left > 0) {
         r = read(socket, buffer, left);
@@ -23,19 +70,82 @@ int receive_message(int socket, request *message) {
     return 0;
 }
 
-int send_message(int socket, request *answer) {
+
+
+int receive_message(int socket, request *message)
+{
+    int type = 0;
+    receive_package(socket, &type, sizeof(int));
+    message->type = ntohl(type);
+    int key = 0;
+    receive_package(socket,&key, sizeof(int));
+    message->key = ntohl(key);
+    ssize_t len_v1 = 0;
+    receive_package(socket, &len_v1, sizeof(long));
+    receive_package(socket, &message->value_1, len_v1);
+    int n2 = 0;
+    receive_package(socket, &n2, sizeof(int));
+    message->N_value_2 = ntohl(n2);
+    for(int i = 0; i< message->N_value_2; i++)
+    {
+        double value = 0;
+        receive_package(socket,&value, sizeof(double));
+        message->value_2[i] = net_to_host_double(value);
+    }
+    int x,y = 0;
+    receive_package(socket, &x, sizeof(int));
+    receive_package(socket, &y, sizeof(int));
+    message->value_3.x = ntohl(x);
+    message->value_3.y = ntohl(y);
+    int answer = 0;
+    receive_package(socket, &answer, sizeof(int));
+    message->answer = ntohl(answer);
+
+    return 0;
+}
+
+int send_package(int socket, void *message, int size)
+{
     int written = 0;
-    int left = sizeof(request);
-    char *buffer = (char *) answer;
+    int left = size;
 
     while (left > 0) {
-        written = write(socket, buffer, left);
+        written = write(socket, message, left);
         if (written <= 0) {
             perror("Error reading from socket");
             return -1;
         }
         left -= written;
-        buffer += written;
+        message += written;
     }
+    return 0;
+}
+
+
+
+int send_message(int socket, request *answer) {
+
+
+    __uint32_t type = htonl(answer->type);
+    send_package(socket, &type, sizeof(int));
+    __uint32_t key = htonl(answer->key);
+    send_package(socket, &key, sizeof(int));
+    size_t len_v1 = strlen(answer->value_1);
+    send_package(socket, &len_v1, sizeof(long));
+
+    send_package(socket, &answer->value_1, len_v1);
+    __uint32_t N_value_2 = htonl(answer->N_value_2);
+    send_package(socket, &N_value_2, sizeof(int));
+    for(int i = 0; i< answer->N_value_2; i++)
+    {
+        __uint64_t conv_double = htonl(answer->value_2[i]);
+        send_package(socket, &conv_double, sizeof(double));
+    }
+    __uint32_t x = htonl(answer->value_3.x);
+    send_package(socket, &x, sizeof(int));
+    __uint32_t y = htonl(answer->value_3.y);
+    send_package(socket, &y, sizeof(int));
+    __uint32_t response = htonl(answer->answer);
+    send_package(socket, &response, sizeof(int));
     return 0;
 }
